962c95be04d9422d76dde2ae1bd6be72f53249e0
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-01 15:26:06 +0900

    Complete Kotlin Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,41 @@
 import helper.util.println
 import helper.util.readInput
+import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val (firstList, secondList) = input.map { line ->
+            line.split(" ")
+                .filter { it.isNotBlank() }
+                .map { it.toLong() }
+        }
+            .map { Pair(it[0], it[1]) }
+            .unzip()
+        val sortedFirstList = firstList.sorted()
+        val sortedSecondList = secondList.sorted()
+
+        return sortedFirstList.zip(sortedSecondList)
+            .sumOf { abs(it.first - it.second) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (firstList, secondList) = input.map { line ->
+            line.split(" ")
+                .filter { it.isNotBlank() }
+                .map { it.toLong() }
+        }
+            .map { Pair(it[0], it[1]) }
+            .unzip()
+
+        val counterMap = firstList.associateWith { 0 }
+            .toMutableMap()
+        secondList.forEach {
+            if (counterMap.containsKey(it)) {
+                counterMap[it] = counterMap[it]!! + 1
+            }
+        }
+
+        return counterMap.entries.sumOf { it.key * it.value }
     }
 
     val input = readInput("Day01")
