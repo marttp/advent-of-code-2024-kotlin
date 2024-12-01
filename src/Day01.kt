29bd494351055b7a42e53c0ef30246b747581a84
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-01 15:13:03 +0900

    Prepare repository structure

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,6 @@
+import helper.util.println
+import helper.util.readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -7,14 +10,6 @@ fun main() {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
