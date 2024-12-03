d71504384e09da5cc9bc597e7a21e8bd9df9ba78
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-03 19:24:42 +0900

    Complete Kotlin Day 03

---
+++ src/Day03.kt
@@ -0,0 +1,50 @@
+import helper.util.println
+import helper.util.readInput
+import java.util.*
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val longText = input.joinToString("")
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        return regex.findAll(longText)
+            .map { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val longText = input.joinToString("")
+        val multiplyRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        val multiplyQueue: LinkedList<MatchResult> = multiplyRegex.findAll(longText)
+            .map { it }
+            .toCollection(LinkedList())
+        val commandRegex = Regex("do(?:n't)?\\(\\)")
+        val commandQueue: LinkedList<MatchResult> = commandRegex.findAll(longText)
+            .map { it }
+            .toCollection(LinkedList())
+
+        var isMultiplyEnabled = true
+        var sum = 0
+
+        while (multiplyQueue.isNotEmpty()) {
+            val nextOperation = multiplyQueue.removeFirst()
+            while (commandQueue.isNotEmpty() && commandQueue.peek().range.first < nextOperation.range.first) {
+                val command = commandQueue.removeFirst()
+                isMultiplyEnabled = command.value == "do()"
+            }
+
+            if (isMultiplyEnabled) {
+                val op1 = nextOperation.groupValues[1].toInt()
+                val op2 = nextOperation.groupValues[2].toInt()
+                sum += op1 * op2
+            }
+        }
+
+        return sum
+    }
+
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
