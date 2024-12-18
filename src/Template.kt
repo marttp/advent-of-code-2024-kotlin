1b0312399d45ea3c9fb98a679d73c273475ffac5
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-18 19:32:04 +0900

    Add template class

---
+++ src/Template.kt
@@ -0,0 +1,17 @@
+import helper.util.println
+import helper.util.readInput
+
+fun main() {
+
+    fun part1(input: List<String>): String {
+        return ""
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+    val input = readInput("Day00")
+    part1(input).println()
+    part2(input).println()
+}
