3c0cbfe79f03f62c6d609b7bb1143c3a39883336
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-18 11:02:38 +0900

    Draft day 17

---
+++ src/Day17.kt
@@ -0,0 +1,21 @@
+import helper.util.println
+import helper.util.readInput
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+fun main() {
+
+    fun part1(input: List<String>): String {
+        return ""
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+    val input = readInput("Day17")
+    part1(input).println()
+    part2(input).println()
+}
